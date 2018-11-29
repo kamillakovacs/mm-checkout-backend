1d96bce9a7276e9024110807e7535af77dba8542
Kamilla Kovacs <knkovacs@gmail.com>
2018-11-29 16:16:49 +0100

    docker stuff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = 'http://ec2-18-234-187-37.compute-1.amazonaws.com:3000';
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}`);
