a8cfd54a1e8c9f033e64e841af6328474a4796fd
mark182182 <versalife11@hotmail.com>
2018-12-04 13:49:17 +0100

    teso

--- app.js
+++ app.js
@@ -75,8 +75,7 @@ app.post('/feedback', jsonParser, (req, res) => {
 })
 
 app.get('/link', (req, res) => {
-  console.log(req.query);
-  const { user_name, channel_name } = req.params;
+  const { user_name, channel_name } = req.query;
   console.log(`${user_name}, ${channel_name}`);
   res.status(200).send(`http://mmcheckoutfrontend.s3-website.eu-central-1.amazonaws.com?channel_name=${channel_name}&username=${user_name} Click on the link to submit your feedback.`);
 });
