306c9ff023b57132bf5d32418d098ecc2e73438b
Kamilla Kovacs <knkovacs@gmail.com>
2018-12-09 21:04:26 +0100

    fix /link

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ app.post('/feedback', jsonParser, (req, res) => {
 });
 
 app.get('/link', (req, res) => {
-  const { user_name, channel_name } = req.query;
-  console.log(user_name, channel_name);
-  res.status(200).send(`http://mmcheckoutfrontend.s3-website.eu-central-1.amazonaws.com?channel_name=${channel_name}&username=${username} Click on the link to submit your feedback.`)
+  const { username, channel_name } = req.query;
+  console.log(`${username}, ${channel_name}`);
+  res.status(200).send(`Click on the link to submit your feedback: http://mmcheckoutfrontend.s3-website.eu-central-1.amazonaws.com?channel_name=${channel_name}&username=${username}`)
 });
 
 app.get('/daily-feedback', (req, res) => {
