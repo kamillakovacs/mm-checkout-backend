3d48479ed2fbacf44728e3e3040d50997e79a7e5
Kamilla Kovacs <knkovacs@gmail.com>
2018-12-13 14:32:08 +0100

    link: das cool lets try with attachment

--- app.js
+++ app.js
@@ -75,7 +75,43 @@ app.post('/link', jsonParser, (req, res) => {
     "trigger_word": "/checkout",
     "user_id": "rnina9994bde8mua79zqcg5hmo",
     "user_name": user_name,
-    "file_ids": "znana9194bde8mua70zqcg5hmo"
+    "file_ids": "znana9194bde8mua70zqcg5hmo", 
+    "attachments": [
+      {
+        "fallback": "test",
+        "color": "#FF8000",
+        "pretext": "This is optional pretext that shows above the attachment.",
+        "text": "This is the text of the attachment. It should appear just above an image of the Mattermost logo. The left border of the attachment should be colored orange, and below the image it should include additional fields that are formatted in columns. At the top of the attachment, there should be an author name followed by a bolded title. Both the author name and the title should be hyperlinks.",
+        "author_name": "Mattermost",
+        "author_icon": "http://www.mattermost.org/wp-content/uploads/2016/04/icon_WS.png",
+        "author_link": "http://www.mattermost.org/",
+        "title": "Example Attachment",
+        "title_link": "http://docs.mattermost.com/developer/message-attachments.html",
+        "fields": [
+          {
+            "short":false,
+            "title":"Long Field",
+            "value":"Testing with a very long piece of text that will take up the whole width of the table. And then some more text to make it extra long."
+          },
+          {
+            "short":true,
+            "title":"Column One",
+            "value":"Testing"
+          },
+          {
+            "short":true,
+            "title":"Column Two",
+            "value":"Testing"
+          },
+          {
+          "short":false,
+          "title":"Another Field",
+          "value":"Testing"
+          }
+        ],
+        "image_url": "http://www.mattermost.org/wp-content/uploads/2016/03/logoHorizontal_WS.png"
+      }
+    ]
   })
   // `Feedback form:\nhttp://mmcheckoutfrontend.s3-website.eu-central-1.amazonaws.com?channel_name=${channel_name}&username=${user_name}`)
 });
